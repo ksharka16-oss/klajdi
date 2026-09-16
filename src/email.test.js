066c4ac3e23650e7fe39da48ca06bc7821add296
ksharka16-oss <ksharka16@gmail.com>
2026-09-16 19:36:44 +0200

    Escludi comunicazioni PagoPA informative: src/email.test.js

--- src/email.test.js
+++ src/email.test.js
@@ -6,6 +6,7 @@ test('una mail normale non diventa fattura',()=>assert.equal(classifyEmail('Ciao
 test('la parola fattura senza allegato resta normale',()=>assert.equal(classifyEmail('Informazioni fattura','Azienda','Nessun documento',[]),'normal'));
 test('fattura con PDF viene riconosciuta',()=>assert.equal(classifyEmail('La tua fattura','Fornitore','Documento allegato',['fattura.pdf']),'invoice'));
 test('una promozione viene ignorata anche se cita fatture',()=>assert.equal(classifyEmail('Offerta sulle fatture','Newsletter','Sconto',['promo.pdf']),'ignore'));
+test('una comunicazione informativa su PagoPA non diventa da pagare',()=>assert.equal(classifyEmail('Istruzioni per scaricare quietanze','Comune','Consulta il portale PagoPA per le quietanze già pagate',[]),'normal'));
 test('una fattura vera senza prova di pagamento risulta da pagare',()=>assert.equal(financialStatus('invoice'),'to_pay'));
 test('una conferma di pagamento risulta pagata',()=>assert.equal(financialStatus('payment_confirmation'),'paid'));
 test('una mail finanziaria debole non viene mostrata',()=>assert.equal(financialStatus(classifyEmail('Documento','Servizio','In allegato',['documento.pdf']),'Documento','Servizio','In allegato',['documento.pdf']),null));
