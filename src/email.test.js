e373fe9e9f73c9a1be6e37e820e70fbdfe44d536
ksharka16-oss <ksharka16@gmail.com>
2026-09-16 19:41:55 +0200

    Riconosci ricevute Apple e Google Play: src/email.test.js

--- src/email.test.js
+++ src/email.test.js
@@ -7,6 +7,7 @@ test('la parola fattura senza allegato resta normale',()=>assert.equal(classifyE
 test('fattura con PDF viene riconosciuta',()=>assert.equal(classifyEmail('La tua fattura','Fornitore','Documento allegato',['fattura.pdf']),'invoice'));
 test('una promozione viene ignorata anche se cita fatture',()=>assert.equal(classifyEmail('Offerta sulle fatture','Newsletter','Sconto',['promo.pdf']),'ignore'));
 test('una comunicazione informativa su PagoPA non diventa da pagare',()=>assert.equal(classifyEmail('Istruzioni per scaricare quietanze','Comune','Consulta il portale PagoPA per le quietanze già pagate',[]),'normal'));
+test('riconosce le ricevute affidabili Apple e Google Play',()=>{assert.equal(classifyEmail('La tua fattura da Apple','Apple <no_reply@email.apple.com>','',[]),'receipt');assert.equal(classifyEmail("Ricevuta dell'ordine Google Play del giorno 25 ago 2026",'Google Play <googleplay-noreply@google.com>','',[]),'receipt')});
 test('una fattura vera senza prova di pagamento risulta da pagare',()=>assert.equal(financialStatus('invoice'),'to_pay'));
 test('una conferma di pagamento risulta pagata',()=>assert.equal(financialStatus('payment_confirmation'),'paid'));
 test('una mail finanziaria debole non viene mostrata',()=>assert.equal(financialStatus(classifyEmail('Documento','Servizio','In allegato',['documento.pdf']),'Documento','Servizio','In allegato',['documento.pdf']),null));
